d43d8a372698fdabc9312e16ff9540c5309b1f05
Manyyack <shreel.chhatbar@gmail.com>
2017-08-13 15:34:35 +0530

    PPhewwwww. New to git and VS. Testing out!

--- File-Sorter/Form1.Designer.cs
+++ File-Sorter/Form1.Designer.cs
@@ -28,12 +28,97 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.Organize = new System.Windows.Forms.Button();
+			this.F_Path = new System.Windows.Forms.RichTextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.Browse = new System.Windows.Forms.Button();
+			this.FolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+			this.SuspendLayout();
+			// 
+			// Organize
+			// 
+			this.Organize.Location = new System.Drawing.Point(532, 11);
+			this.Organize.Name = "Organize";
+			this.Organize.Size = new System.Drawing.Size(122, 21);
+			this.Organize.TabIndex = 0;
+			this.Organize.Text = "Organize";
+			this.Organize.UseVisualStyleBackColor = true;
+			this.Organize.Click += new System.EventHandler(this.Connect_Click);
+			// 
+			// F_Path
+			// 
+			this.F_Path.AcceptsTab = true;
+			this.F_Path.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.F_Path.AutoWordSelection = true;
+			this.F_Path.BackColor = System.Drawing.SystemColors.Info;
+			this.F_Path.DetectUrls = false;
+			this.F_Path.EnableAutoDragDrop = true;
+			this.F_Path.Location = new System.Drawing.Point(47, 12);
+			this.F_Path.Multiline = false;
+			this.F_Path.Name = "F_Path";
+			this.F_Path.Size = new System.Drawing.Size(447, 20);
+			this.F_Path.TabIndex = 1;
+			this.F_Path.Text = "";
+			this.F_Path.TextChanged += new System.EventHandler(this.F_Path_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 12);
+			this.label1.MaximumSize = new System.Drawing.Size(40, 80);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(29, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Path";
+			// 
+			// Browse
+			// 
+			this.Browse.Location = new System.Drawing.Point(500, 10);
+			this.Browse.Name = "Browse";
+			this.Browse.Size = new System.Drawing.Size(26, 23);
+			this.Browse.TabIndex = 3;
+			this.Browse.Text = "...";
+			this.Browse.UseVisualStyleBackColor = true;
+			this.Browse.Click += new System.EventHandler(this.Browse_Click);
+			// 
+			// FolderBrowser
+			// 
+			this.FolderBrowser.ShowNewFolderButton = false;
+			// 
+			// Form1
+			// 
+			this.AcceptButton = this.Organize;
+			this.AllowDrop = true;
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.AutoSize = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(666, 53);
+			this.Controls.Add(this.Browse);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.F_Path);
+			this.Controls.Add(this.Organize);
+			this.Cursor = System.Windows.Forms.Cursors.Default;
+			this.HelpButton = true;
+			this.Name = "Form1";
+			this.Opacity = 0.9D;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "File Sorter";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button Organize;
+		private System.Windows.Forms.RichTextBox F_Path;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button Browse;
+		private System.Windows.Forms.FolderBrowserDialog FolderBrowser;
 	}
 }
 
--- File-Sorter/Form1.cs
+++ File-Sorter/Form1.cs
@@ -1,20 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
 
 namespace File_Sorter
 {
 	public partial class Form1 : Form
 	{
+		String D_Path;
+		
+
 		public Form1()
 		{
-			InitializeComponent();
+			InitializeComponent();			
 		}
+
+		private void Connect_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				Directory.SetCurrentDirectory(F_Path.Text);
+			}
+			catch (ArgumentException)
+			{
+				MessageBox.Show("Enter the Valid Path");
+				return;
+			}
+
+			IEnumerable<string> fileEntries = Directory.EnumerateFiles(Directory.GetCurrentDirectory());
+
+			foreach (string fileName in fileEntries)
+			{
+				var f_Pos = fileName.LastIndexOf('.');
+				string f_Type = fileName.Substring(f_Pos + 1).ToUpper();
+
+				if (Directory.Exists(D_Path + "\\" + f_Type) == false)
+				{
+					Directory.CreateDirectory(D_Path + "\\" + f_Type);
+				}
+				Directory.Move(fileName, fileName.Insert(fileName.LastIndexOf("\\") + 1, f_Type + "\\"));
+			}
+		}
+		private void F_Path_TextChanged(object sender, EventArgs e)
+		{
+			D_Path = F_Path.Text;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			ToolTip tooltip1 = new ToolTip();
+
+			tooltip1.AutoPopDelay = 5000;
+			tooltip1.InitialDelay = 500;
+			tooltip1.ReshowDelay = 500;
+			tooltip1.ShowAlways = true;
+
+			tooltip1.SetToolTip(this.F_Path, "Enter the path of the directory which needs sorting of the files");
+			tooltip1.SetToolTip(this.Organize, "Click to sort files");
+			tooltip1.SetToolTip(this.Browse, "Browse the folder");
+		}
+
+		private void Browse_Click(object sender, EventArgs e)
+		{
+			FolderBrowser.ShowDialog();
+			D_Path = FolderBrowser.SelectedPath;
+			Console.WriteLine(D_Path);
+			F_Path.Text = D_Path;
+			FolderBrowser.Dispose();
+		}
+
+			
 	}
 }
+		
